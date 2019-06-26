0a65bf4d5a361bc6e52a9064f465e537b7bcfdb1
arkbrych <arkbrych@gmail.com>
2019-06-26 22:58:26 +0200

    Exercice for List collection

---
+++ src/app/Logic.java
@@ -0,0 +1,47 @@
+package app;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * @author Arkadiusz
+ * class is adding number using recursion
+ */
+public class Logic {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        StringBuilder builder = new StringBuilder();
+        Integer sum = 0;
+
+        NumbersReader(list);
+
+        for (Integer all : list
+        ) {
+            sum = sum + all;
+        }
+        for (int i = 0; i < list.size(); i++) {
+            builder.append(i);
+            if (i != (list.size() - 1)) {
+                builder.append(" + ");
+            }
+        }
+        System.out.println(builder.toString() + " = " + sum);
+    }
+
+    private static List<Integer> NumbersReader(List<Integer> list) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Write number");
+        String number = input.next();
+
+        if (number.equalsIgnoreCase("exit")) {
+            input.close();
+            return list;
+        } else {
+            list.add(Integer.parseInt(number));
+            NumbersReader(list);
+        }
+        input.close();
+        return list;
+    }
+}
